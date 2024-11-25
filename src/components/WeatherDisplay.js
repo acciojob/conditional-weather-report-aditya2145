f14860f023e7af77ed3dba1cc3e9e114c2ea871b
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-11-25 11:07:22 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const WeatherDisplay = ({weatherState}) => {
+const WeatherDisplay = ({ weatherState }) => {
   return (
     <div>
         <p>Temperature : <span style={{color: weatherState.temperature > 20 ? "red" : "blue"}} >{weatherState.temperature}</span></p>
